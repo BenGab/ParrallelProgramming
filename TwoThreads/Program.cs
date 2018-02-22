668b0e630d16be8481b071827574ded28030dd77
Benkő Gábor <benkogabor@msn.com>
2018-02-22 18:39:19 +0100

    second task with 2 threads

---
+++ TwoThreads/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwoThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting 2 threads");
+            var t1 = new Thread(ThreadMethod1);
+            var t2 = new Thread(ThreadMethod2);
+            t1.Start();
+            t2.Start();
+        }
+
+        static void ThreadMethod1()
+        {
+            while (true)
+            {
+                Console.WriteLine("*********");
+                Thread.Sleep(300);
+            }
+        }
+
+        static void ThreadMethod2()
+        {
+            while (true)
+            {
+                Console.WriteLine("sdjkkdlsfjlék");
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
