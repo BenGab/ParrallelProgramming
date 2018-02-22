832e3f2edd45180a0faabf7c5cc915b168f65a15
Benkő Gábor <benkogabor@msn.com>
2018-02-22 18:29:02 +0100

    first parallel program

---
+++ ThreadBasics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating new Thread directly");
+            Console.WriteLine($"Main thread number: {Thread.CurrentThread.GetHashCode()}");
+            Thread aThread = new Thread(ThreadMethod);
+            aThread.Start();
+            
+        }
+
+        static void ThreadMethod()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} number: {Thread.CurrentThread.GetHashCode()}");
+        }
+    }
+}
