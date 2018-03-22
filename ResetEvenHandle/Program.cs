2f37b65509c4c0a9cbb3aa2ab60cef6580abe20f
Benkő Gábor <benkogabor@msn.com>
2018-03-22 19:23:58 +0100

    Autoresetevent example

---
+++ ResetEvenHandle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResetEvenHandle
+{
+    class Program
+    {
+        static EventWaitHandle wh = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            var thread = new Thread(new ThreadStart(Waiter));
+            thread.Start();
+            wh.Set();
+        }
+
+        static void Waiter()
+        {
+            Console.WriteLine("Waiting ...");
+            wh.WaitOne();
+            Console.WriteLine("Notified ...");
+        }
+    }
+}
