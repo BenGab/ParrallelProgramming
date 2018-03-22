9bbab75b94bd1a0b6358a778d590684e95b57b6d
Benkő Gábor <benkogabor@msn.com>
2018-03-22 19:20:07 +0100

    Producer Consumer example

---
+++ ProducerConsumer/Buffer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class Buffer
+    {
+        const int size = 16;
+        char[] buff = new char[size];
+        int head = 0, tail = 0, n = 0;
+        object mutext = new object();
+
+        public void Put(char ch)
+        {
+            lock (mutext)
+            {
+                while (n == size)
+                    Monitor.Wait(mutext);
+
+                buff[tail] = ch;
+                tail = (tail + 1) % size;
+                ++n;
+                Monitor.Pulse(mutext);
+
+            }
+        }
+
+        public char Get()
+        {
+            lock (mutext)
+            {
+                while (n == 0)
+                    Monitor.Wait(mutext);
+
+                char ch = buff[head];
+                head = (head + 1) % size;
+                --n;
+                Monitor.Pulse(mutext);
+                return ch;
+            }
+        }
+    }
+}
---
+++ ProducerConsumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = new Buffer();
+
+            for (int i = 0; i < 7; i++)
+            {
+                var thread = new Thread(new ParameterizedThreadStart((object ch) =>
+                {
+                    buffer.Put(ch.ToString()[0]);
+                }));
+                thread.Start(i);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                var thread = new Thread(new ThreadStart(() =>
+                {
+                    Console.WriteLine(buffer.Get());
+                }));
+                thread.Start();
+            }
+
+        }
+
+
+    }
+}
