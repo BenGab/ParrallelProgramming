979a54f274bba105054fc859d427c53e9c1da5aa
Gábor Benkő <benkogabor@msn.com>
2018-04-12 18:36:47 +0200

    async await test

---
+++ AsynAwaitConsoleDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynAwaitConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first");
+            Task.Run(async () =>
+            {
+                Console.WriteLine(await ResultAsync());
+            });          
+            Console.WriteLine("last");
+            Console.ReadLine();
+        }
+
+        static string Result()
+        {
+            Thread.Sleep(3000);
+            return "Hello World";
+        }
+
+        static async Task<string> ResultAsync()
+        {
+            await Task.Delay(3000);
+            return "Hello World";
+        }
+    }
+}
