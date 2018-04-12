bbbefdfaa913d53c74fd72ed75633a165895f0ae
Gábor Benkő <benkogabor@msn.com>
2018-04-12 18:57:22 +0200

    Wikipedia Article Analyzer

--- WikiediaArticleAnalyzer/Program.cs
+++ WikiediaArticleAnalyzer/Program.cs
@@ -17,15 +17,27 @@ namespace WikiediaArticleAnalyzer
 
         static void TestWrite()
         {
+            string cont = string.Empty;
             const string keyword = "John von Neumann";
-            XDocument xd = XDocument.Load(
-                $"https://en.wikipedia.org/w/api.php?action=query&titles={keyword}&prop=links&&pllimit=max&format=xml");
-            Console.WriteLine(xd.Descendants("pl").Count());
-
-            foreach(var xelem in xd.Descendants("pl").Select(x => x.Attribute("title").Value))
+            do
             {
-                Console.WriteLine(xelem);
-            }
+                XDocument xd = XDocument.Load(
+                    $"https://en.wikipedia.org/w/api.php?action=query&titles={keyword}&prop=links&&pllimit=max&format=xml" + cont);
+                Console.WriteLine(xd.Descendants("pl").Count());
+                if(xd.Descendants("continue").Count() > 0)
+                {
+                    cont = $"&plcontinue={xd.Descendants("continue").Single().Attribute("plcontinue").Value}";
+                }
+                else
+                {
+                    cont = string.Empty;
+                }
+
+                foreach (var xelem in xd.Descendants("pl").Select(x => x.Attribute("title").Value))
+                {
+                    Console.WriteLine(xelem);
+                }
+            } while (!string.IsNullOrEmpty(cont));
         }
     }
 }
